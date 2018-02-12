72ad3b66ba0b583a8d0fa92a4e551c779454436e
michael <michaelreeves808@gmail.com>
2018-02-12 05:37:10 -1000

    Initial Commit

--- SwearingIsFun/MainForm.Designer.cs
+++ SwearingIsFun/MainForm.Designer.cs
@@ -53,7 +53,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(514, 95);
+            this.button1.Location = new System.Drawing.Point(501, 97);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
